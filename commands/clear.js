f11bcb030a9417d44d28cf199f21a409198f3354
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2019-08-12 17:07:32 +0500

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,7 @@ const Discrod = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Sorry but you don't have permission");
-  if(!args[0]) return message.channel.send("**USAGE: #purge <number of messages to delete>");
+  if(!args[0]).then(message.delete()) return message.channel.send("**USAGE: #purge <number of messages to delete>**");
   message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
   });
