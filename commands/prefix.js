211266e4b702e90bc3f93cf1ea02e61ae10c346a
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2019-08-12 16:44:46 +0500

    Add files via upload

---
+++ commands/prefix.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+module.exports.run = async (bot, message, args) => {
+
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
+  if(!args[0] || args[0 == "help"]) return message.reply(`Usage: $prefix <desired prefix here>` );
+
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+
+  prefixes[message.guild.id] = {
+    prefixes: args[0]
+  };
+
+  fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+    if(err) console.log(err)
+  });
+let pEmbed = new Discord.RichEmbed()
+.addField("New Prefix", `${args[0]} is the new prefix`)
+.setColor("#00FF00")
+
+message.channel.send(pEmbed);
+}
+
+
+module.exports.help = {
+  name: "prefix"
+}
